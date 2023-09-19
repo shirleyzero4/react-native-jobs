ef32db30330098f9bab0373415ab642fdb255bb9
shirleyzero4 <shirley.c.nguyen@gmail.com>
2023-09-19 16:44:05 -0700

    Updated home screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,24 @@
-import {Slot} from "expo-router";
+import {Stack} from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
-    return <Slot />;
-};
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
+    })
+
+    const onLayoutRootView = useCallback(async () => {
+        if(fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded])
+
+    if(!fontsLoaded) return null;
+
+    return <Stack onLayout={onLayoutRootView} />;
+}
